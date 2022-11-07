cc33b8c85f973261c5cae7969b40429630f95cac
Anurag <78105107+anu3dev@users.noreply.github.com>
2022-11-07 15:17:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import {iterateTwoArray, searchFromURL, protoOne, protoTwo} from './utils'
+
+export default function App() {
+
+    iterateTwoArray()
+    searchFromURL()
+    protoOne()
+    protoTwo()
+  
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+    </div>
+  );
+}
